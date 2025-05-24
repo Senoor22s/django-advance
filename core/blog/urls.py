5d8dababc023ef5dde4132c92eeac754cd4c5859
Senoor22s <legendsdt2@gmail.com>
2025-05-24 16:14:44 +0330

    6
    ...

--- core/blog/urls.py
+++ core/blog/urls.py
@@ -10,4 +10,8 @@ urlpatterns=[
     path('go-to-maktabkhooneh/<int:pk>/',Redirectview.as_view(),name='redirect-to-maktabkhooneh'),
     path('post/',PostList.as_view(),name='post-list'),
     path('post/<int:pk>/',PostDetail.as_view(),name='post-detail'),
+    path('post/create',PostCreate.as_view(),name='post-create'),
+    path('post/create2',PostCreateView.as_view(),name='post-create2'),
+    path('post/<int:pk>/edit',PostEdit.as_view(),name='post-edit'),
+    path('post/<int:pk>/delete',PostDelete.as_view(),name='post-delete'),
 ]
